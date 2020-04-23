e314e88bd0fd0abd69efc6fcbe35a0154a3ac55c
onurbayraktar <obayraktar17@gmail.com>
2020-04-23 16:10:00 +0300

    Feat: Data splitting, model const and eval for Dec. Tree and NB.

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import sqlite3
-import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-from scipy import stats
-from sklearn import preprocessing
+from sklearn.model_selection import train_test_split, KFold
+from sklearn import tree, metrics
+from sklearn.naive_bayes import GaussianNB
+
 
 COLUMNS = ["AF3", "F7", "F3", "FC5", "T7", "P7", "O1",
             "O2", "P8", "T8", "FC6", "F4", "F8", "AF4", "STATE"]
@@ -64,15 +65,63 @@ def outlierHandling(df):
     return df
 
 
+# Function that splits the data into train / test pairs. #
+def generateTrainTestData(df):
+    y_data = df['eyeDetection']
+    x_data = df.drop(['eyeDetection'], axis=1)
+
+    x_train, x_test, y_train, y_test = train_test_split(x_data, y_data, test_size=0.2, shuffle=True)
+    trainTestData = [x_train, x_test, y_train, y_test]
+    return trainTestData
+
+
+def applyDecisionTreeAlgorithm(trainTestData):
+    x_train = trainTestData[0]
+    y_train = trainTestData[2]
+    x_test = trainTestData[1]
+    y_test = trainTestData[3]
+
+    classifier = tree.DecisionTreeClassifier(criterion="entropy")
+    classifier = classifier.fit(x_train,y_train)
+    prediction = classifier.predict(x_test)
+    result = [y_test, prediction]
+    return result
+
+
+def applyNaiveBayesAlgorithm(trainTestData):
+    x_train = trainTestData[0]
+    y_train = trainTestData[2]
+    x_test = trainTestData[1]
+    y_test = trainTestData[3]
+
+    classifier = GaussianNB()
+    classifier.fit(x_train, y_train)
+    prediction = classifier.predict(x_test)
+    result = [y_test, prediction]
+    return result
+
+
+def evaluation(result):
+    testedValues = result[0]
+    predictedValues = result[1]
+    accuracy = metrics.accuracy_score(testedValues, predictedValues)
+    print("Accuracy: " + str(accuracy))
+
+
 def main():
     database = "EyeState.db"
     fetchQuery = "SELECT * FROM States"
 
     conn = createConnectionWithDB(database)
     dataFrame = createDataFrame(conn, fetchQuery)
-    plotTheDataDistribution(dataFrame)              # The distribution before outlier handling.
+    #plotTheDataDistribution(dataFrame)              # The distribution before outlier handling.
     dataFrame = outlierHandling(dataFrame)
-    plotTheDataDistribution(dataFrame)              # The distribution after outlier handling.
+    #plotTheDataDistribution(dataFrame)              # The distribution after outlier handling.
+    trainTestData = generateTrainTestData(dataFrame)
+    results = applyDecisionTreeAlgorithm(trainTestData)
+    #results = applyNaiveBayesAlgorithm(trainTestData)
+    evaluation(results)
+
 
 
 if __name__ == '__main__':
